ca9797af261451b64b719009ba1d49a9e618c03e
Muhammad Umair <malikumair112233@gmail.com>
2024-08-06 12:36:28 +0500

    Fix use Auth

--- frontend/src/services/BACKEND/useAuth.ts
+++ frontend/src/services/BACKEND/useAuth.ts
@@ -1,63 +1,58 @@
-"use server";
-import axios from "axios";
-import { cookies } from "next/headers";
+'use server'
+import axios from 'axios'
+import { cookies } from 'next/headers';
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-export const login = async ({
-  username,
-  password,
-}: {
-  username: string;
-  password: string;
-}): Promise<{ success: boolean }> => {
-  let response;
-  try {
-    response = await axios.post(apiUrl + "/login", { username, password });
+export const login = async ({ username, password }: { username: string, password: string }): Promise<{ success: boolean }> => {
 
-    if (response.status === 200) {
-      cookies().set("access", response.data.access_token, {
-        maxAge: 3600 * 24 * 2,
-      });
-      return { success: true };
-    } else {
-      return { success: false };
+    let response;
+    try {
+        response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND}` + "/login", { username, password })
+
+
+        if (response.status === 200) {
+            cookies().set("access", response.data.access_token, { maxAge: 3600 * 24 * 2 })
+            return { success: true }
+        } else {
+            return { success: false }
+        }
+    } catch (e: any) {
+        console.log("Error on /login >>>>>>")
+        console.log(e.response ?? e)
+        console.log(">>>>>>>>>>>>>>>>>>>>>>")
+        return {
+            success: false,
+        }
     }
-  } catch (e: any) {
-    console.log("Error on /login >>>>>>");
-    console.log(e.response ?? e);
-    console.log(">>>>>>>>>>>>>>>>>>>>>>");
-    return {
-      success: false,
-    };
-  }
-};
-export const register = async ({
-  username,
-  password,
-}: {
-  username: string;
-  password: string;
-}) => {
-  let response;
-  try {
-    response = await axios.post(apiUrl + "/register", { username, password });
 
-    if (response.status === 200) {
-      return { success: true };
-    } else {
-      return { success: false };
+
+
+}
+export const register = async ({ username, password }: { username: string, password: string }) => {
+    let response;
+    try {
+        response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND}` + "/register", { username, password })
+
+
+
+        if (response.status === 200) {
+            return { success: true }
+        } else {
+            return { success: false }
+        }
+    } catch (e: any) {
+        console.log("Error on /register >>>>>>")
+        console.log(e.response ?? e)
+        console.log(">>>>>>>>>>>>>>>>>>>>>>")
+        return {
+            success: false,
+        }
     }
-  } catch (e: any) {
-    console.log("Error on /register >>>>>>");
-    console.log(e.response ?? e);
-    console.log(">>>>>>>>>>>>>>>>>>>>>>");
-    return {
-      success: false,
-    };
-  }
-};
+
+
+}
+
 
 export const logout = async () => {
-  cookies().delete("access");
-};
+    cookies().delete("access")
+}
